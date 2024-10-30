58217728069aac97d7d567492460117788ee083b
Naima Rahman <95029959+Naima006@users.noreply.github.com>
2024-10-30 19:20:13 +0600

    Delete difficulty.py

--- difficulty.py
+++
@@ -1,6 +0,0 @@
-class Difficulty:
-    def __init__(self, label, timeleft, difficulty_multiplier, colors):
-        self.label = label
-        self.timeleft = timeleft
-        self.difficulty_multiplier = difficulty_multiplier #score multiplier
-        self.colors = colors
